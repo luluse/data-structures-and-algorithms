7933543d1e8e0741292978240847f3fc5a4b8b44
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-09-29 20:21:55 -0700

    get edge challenge solved

--- Javascript/challenges/breadthFirst/breadthFirst.test.js
+++ Javascript/challenges/breadthFirst/breadthFirst.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const Graph = require('../../data-structures/graph/graph');
-const Queue = require('../../data-structures/stacksAndQueues/stacks-and-queues')
+const { Graph } = require('../../data-structures/graph/graph');
+const { Queue } = require('../../data-structures/stacksAndQueues/stacks-and-queues');
 
 
 class BreadthFirstGraph extends Graph{
@@ -10,18 +10,18 @@ class BreadthFirstGraph extends Graph{
   }
 
   breadthFirst(startNode){
-    const nodes = new Map()
-    const breadth = new Queue()
+    const nodes = new Set();
+    const breadth = new Queue();
     const visitedArr = [];
 
-    breadth.enqueue(startNode)
-
-    while (breadth){
+    breadth.enqueue(startNode);
+    console.log(breadth, 'this is my breadth')
+    while (breadth.length){
 
       const front = breadth.dequeue();
       nodes.add(front);
-      visitedArr.push(front);
-
+      visitedArr.push(front.value);
+      console.log('visited Arr', visitedArr)
       const children = this.getNeighbors(front);
 
       for (let child of children){
@@ -33,9 +33,12 @@ class BreadthFirstGraph extends Graph{
         }
       }
     }
-    return nodes;
+    
+    return visitedArr;
   }
 }
+
+module.exports = BreadthFirstGraph;
 
 //-------- TESTS --------
 
@@ -44,21 +47,21 @@ it('BreadthFirstGraph Should be defined', ()=>{
 });
 
 it('Should return the list of nodes visited in the right order', ()=>{
-  const graph = new Graph()
+  const graph = new BreadthFirstGraph()
 
-  graph.addNode('A');
-  graph.addNode('B');
-  graph.addNode('C');
-  graph.addNode('D');
-  graph.addNode('E');
-  graph.addNode('f');
+  const a = graph.addNode('A');
+  const b = graph.addNode('B');
+  const c = graph.addNode('C');
+  const d = graph.addNode('D');
+  const e = graph.addNode('E');
+  const f = graph.addNode('f');
 
-  graph.addEdge('A','B');
-  graph.addEdge('B','C');
-  graph.addEdge('B','D');
-  graph.addEdge('C','E');
-  graph.addEdge('C','F');
-  graph.addEdge('D','F');
+  graph.addEdge(a,b);
+  graph.addEdge(b,c);
+  graph.addEdge(b,d);
+  graph.addEdge(c,e);
+  graph.addEdge(c,f);
+  graph.addEdge(d,f);
 
-  expect(new Map()).toBe('A', 'B', 'C', 'D', 'E', 'F')
+  expect(graph.breadthFirst(a)).toEqual(['A', 'B', 'C', 'D', 'E', 'F'])
 });
